bd86de5d7745bb0c076d47b273a9c2d1f4a55fcd
Thành <hochauthanh@fukuda.ai>
2026-03-30 16:35:07 +0700

    feat:TimKiem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import PhamDucHuyTrangChu from "./components/PhamDucHuyTrangChu";
 import DangKy from "./components/DangKy";
 import DangNhap from "./components/DangNhap";
 import ThongTinUser from "./components/ThongTinUser";
+import TimKiem from "./components/TimKiem";
 
 function App() {
   const [view, setView] = useState("home");
@@ -19,7 +20,11 @@ function App() {
     return <ThongTinUser onBack={() => setView("home")} />;
   }
 
-  return <PhamDucHuyTrangChu onUserClick={() => setView("profile")} />;
+  if (view === "search") {
+    return <TimKiem onBackHome={() => setView("home")} onUserClick={() => setView("profile")} />;
+  }
+
+  return <PhamDucHuyTrangChu onUserClick={() => setView("profile")} onSearchClick={() => setView("search")} />;
 }
 
 export default App;
